eac1ef28699b2dabd72b2dc70be6e179a3312e43
Gardel <gardel741@outlook.com>
2025-12-07 17:52:41 +0800

    fix: 分析记忆时修复引号内容

--- src/memory_graph/short_term_manager.py
+++ src/memory_graph/short_term_manager.py
@@ -11,6 +11,7 @@ import asyncio
 import json
 import re
 import uuid
+import json_repair
 from pathlib import Path
 from typing import Any
 
@@ -530,7 +531,7 @@ class ShortTermMemoryManager:
             json_str = re.sub(r"//.*", "", json_str)
             json_str = re.sub(r"/\*.*?\*/", "", json_str, flags=re.DOTALL)
 
-            data = json.loads(json_str)
+            data = json_repair.loads(json_str)
             return data
 
         except json.JSONDecodeError as e:
